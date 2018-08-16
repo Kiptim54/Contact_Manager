5bca86762e50138d361906e2ac8bebd6fa83a32f
Kiptim <kiptim54@gmail.com>
2018-08-16 09:23:04 +0300

    immuntable input fields working

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -10,27 +10,33 @@ class AddContact extends Component {
         }
     
     }
+    Change = (e)=>{
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+
+    handleSubmit= (e)=>{
+        e.preventDefault();
+        alert(`You have submitted ${this.state.name}`)
+    }
   
   render() {
       const {name, email, phone}= this.state
-      const Change = (e)=>{
-          this.setState({
-              [e.target.name]:e.target.value
-          })
-      }
+      
     return (
       <div>
         <div className="card" style={{width:'50%', padding:'3%', background:'grey light',marginRight:'auto', marginLeft:'auto'}}>
         <span className="card-title">Add Contact</span>
-        <form style={{padding:'2%'}}>
+        <form style={{padding:'2%'}} method="POST" onSubmit={this.handleSubmit}>
             <label htmlFor="name">Name:
-            <input type="text" placeholder="Enter Name" name="name" value={name} onChange={this.Change}/>
+            <input type="text" placeholder="Enter Name" name="name" value={name} onChange={this.Change} required/>
             </label>
             <label htmlFor="name">Email:
-            <input type="email" placeholder="Enter email" name="email" value={email} onChange={this.Change}/>
+            <input type="email" placeholder="Enter email" name="email" value={email} onChange={this.Change} required/>
             </label>
             <label htmlFor="name">Phone:
-            <input type="text" placeholder="Enter Phone" name="Phone" value={phone} onClick={this.Change}/>
+            <input type="text" placeholder="Enter Phone" name="phone" value={phone} onChange={this.Change} required/>
             </label>
             <input type="submit" value="Save Contact" className="btn"  />
         </form>
