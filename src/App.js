50100852c337519fcb4204592d080fc326c2762e
Kiptim <kiptim54@gmail.com>
2018-08-16 11:52:14 +0300

    added api call

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
 import About from './components/pages/About';
 import NotFound from './components/pages/Notfound';
+import Test from './components/pages/Test';
 
 
 import {Provider } from './Context';
@@ -25,6 +26,8 @@ class App extends Component {
         <Route exact path ='/' component={Contacts}/>
         <Route exact path ='/about' component={About}/>
         <Route exact path ='/contact/add' component={AddContact}/>
+        <Route exact path ='/test' component={Test}/>
+        
         <Route component={NotFound}/>
         
       </Switch>
